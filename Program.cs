ffa2e7f0d2195a1077abbcbbf8f36fbb898138d5
WTDemmy <omoluwafemi1@buffs.wtamu.edu>
2024-09-05 12:38:42 -0500

    Add files via upload
    Homework 1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // CODE FOR HOMEWORK 1 
+        // Write out the value for x and y using double due to decimal numbers 
+
+        double x = 2.5;
+        double y = 3.3; 
+
+        // calculating for the x and y 
+
+        double x2 = 4*x*x; // 4*2.5*2.5 = 25
+        double y2 = 3*y;  // 3*3.3 = 9.9 
+
+        double z = x2 + y2; 
+
+        // Using formatted string to print results
+
+        Console.WriteLine($" The value of Z is {z}");
+
+
+
+
+
+        
+    }
+}
